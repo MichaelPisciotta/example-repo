d121799fc7c0db1df7f1110ec7d28ab10a40cb26
MichaelPisciotta <mfp56@miami.edu>
2021-06-20 17:44:18 -0400

    done for now

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+let test = document.getElementById("test");
 
-document.querySelector('body').appendChild(h2); 
+test.addEventListener("mouseenter", function( event ) {
+    // highlight the mouseenter target
+    event.target.style.color = "purple";
+  
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+  
+  // This handler will be executed every time the cursor
+  // is moved over a different list item
+  test.addEventListener("mouseover", function( event ) {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+  
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
